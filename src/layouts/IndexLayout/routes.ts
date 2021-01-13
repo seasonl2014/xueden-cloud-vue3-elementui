787262a8d8c25041c466f69f5afac76fb391faec
goodym <11184629@qq.com>
2021-01-13 12:43:45 +0800

    添加其他文件

--- src/layouts/IndexLayout/routes.ts
+++ src/layouts/IndexLayout/routes.ts
@@ -27,7 +27,7 @@ const IndexLayoutRoutes: Array<RoutesDataItem> = [
     ],
   },
 
-  /*{
+  {
     icon: 'page',
     title: 'index-layout.menu.system',
     path: '/system',
@@ -64,140 +64,8 @@ const IndexLayoutRoutes: Array<RoutesDataItem> = [
         ],
       },
     ],
-  },*/
+  },
 
- /* {
-    icon: 'components',
-    title: 'index-layout.menu.component',
-    path: '/component',
-    redirect: '/component/icon/svg',
-    component: BlankLayout,
-    children:[
-        {
-            icon: 'icon',
-            title: 'index-layout.menu.component.icon',
-            path: 'icon',
-            redirect: '/component/icon/svg',
-            component: BlankLayout,
-            children: [
-                {
-                    title: 'index-layout.menu.component.icon.svg',
-                    path: 'svg',
-                    component: () => import('@/views/component/icon/svg/index.vue'),
-                },
-                {
-                  title: 'index-layout.menu.component.icon.font',
-                  path: 'font',
-                  component: () => import('@/views/component/icon/font/index.vue'),
-                },
-            ]
-        },
-        {
-            icon: 'editor',
-            title: 'index-layout.menu.component.editor',
-            path: 'editor',
-            redirect: '/component/editor/tuieditor',
-            component: BlankLayout,
-            children: [
-                {
-                    title: 'index-layout.menu.component.editor.tui-editor',
-                    path: 'tuieditor',
-                    component: () => import('@/views/component/editor/tui-editor/index.vue'),
-                },
-                {
-                    title: 'index-layout.menu.component.editor.ckeditor',
-                    path: 'ckeditor',
-                    component: () => import('@/views/component/editor/ckeditor/index.vue'),
-                }
-            ]
-        }
-    ]
-  },*/
-
-/*  {
-    icon: 'page',
-    title: 'index-layout.menu.pages',
-    path: '/pagesample',
-    redirect: '/pagesample/list/table',
-    component: BlankLayout,
-    children: [
-      {
-        icon: 'list',
-        title: 'index-layout.menu.pages.list',
-        path: 'list',
-        redirect: '/pagesample/list/table',
-        component: BlankLayout,
-        children: [
-          {
-            title: 'index-layout.menu.pages.list.table',
-            path: 'table',
-            component: ()=> import('@/views/pagesample/list/table/index.vue'),
-          },
-          {
-            title: 'index-layout.menu.pages.list.highly-adaptive-table',
-            path: 'highlyadaptivetable',
-            component: ()=> import('@/views/pagesample/list/highly-adaptive-table/index.vue'),
-          },
-          {
-            title: 'index-layout.menu.pages.list.search',
-            path: 'search',
-            redirect: '/pagesample/list/search/table',
-            component: BlankLayout,
-            children: [
-              {
-                title: 'index-layout.menu.pages.list.search.table',
-                path: 'table',
-                component: ()=> import('@/views/pagesample/list/search/table/index.vue'),
-              }
-            ],
-          },
-        ],
-      },
-      {
-        icon: 'edit',
-        title: 'index-layout.menu.pages.form',
-        path: 'form',
-        redirect: '/pagesample/form/basic',
-        component: BlankLayout,
-        children: [
-          {
-            title: 'index-layout.menu.pages.form.basic',
-            path: 'basic',
-            component: ()=> import('@/views/pagesample/form/basic/index.vue'),
-          },
-          {
-            title: 'index-layout.menu.pages.form.complex',
-            path: 'complex',
-            component: ()=> import('@/views/pagesample/form/complex/index.vue'),
-          },
-        ],
-      },
-      {
-        icon: 'detail',
-        title: 'index-layout.menu.pages.detail',
-        path: 'detail',
-        redirect: '/pagesample/detail/basic',
-        component: BlankLayout,
-        children: [
-          {
-            title: 'index-layout.menu.pages.detail.basic',
-            path: 'basic',
-            component: ()=> import('@/views/pagesample/detail/basic/index.vue'),
-          },
-          {
-            title: 'index-layout.menu.pages.detail.module',
-            path: 'module',
-            component: ()=> import('@/views/pagesample/detail/module/index.vue'),
-          },
-          {
-            title: 'index-layout.menu.pages.detail.table',
-            path: 'table',
-            component: ()=> import('@/views/pagesample/detail/table/index.vue'),
-          },
-        ],
-      },
-    ],
-  },*/
 
   /*{
     icon: 'permissions',
