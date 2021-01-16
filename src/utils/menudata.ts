8ac1d6ab963ecf8eee73c6768b7fb50c9a6c7177
goodym <11184629@qq.com>
2021-01-16 11:44:13 +0800

    添加其他文件

--- src/utils/menudata.ts
+++ src/utils/menudata.ts
@@ -1,11 +1,15 @@
 /**
- * 封装菜单树形结构
+ * 封装菜单树形结构 基本操作
  * @param {any} array
  * @returns {Array}
  * @author LiQingSong
  */
+
+import settings from '@/config/settings';
 import {MenuTreeDataType} from "@/views/system/menu/list/data";
 import {RoutesDataItem} from "@/utils/routes";
+import IndexLayoutRoutes from "@/layouts/IndexLayout/routes";
+
 
 // 生成用户所具有的树形菜单
 export function generateUserMenusTree<T>(dataMenus: Array<T>): Array<RoutesDataItem> {
@@ -36,7 +40,7 @@ export function generateMenusTree<T>(dataMenus: Array<T>): Array<MenuTreeDataTyp
 
   for (let index = 0, len = dataMenus.length; index < len; index += 1) {
     const menu: any =  dataMenus[index]
-    console.info("menudata遍历数据：",menu.parentId)
+    // console.info("menudata遍历数据：",menu.parentId)
     if(menu.parentId===null){ //判断是否为顶层节点
       const parentMenu: MenuTreeDataType= {
         value: menu.id,
@@ -51,5 +55,57 @@ export function generateMenusTree<T>(dataMenus: Array<T>): Array<MenuTreeDataTyp
 
   }
   return menusList;
+}
+
+
+// 获取左边子菜单函数
+export const  getLeftMenuChildren = (array: any) => {
+  const menuChildren: any = [];
+ /* for (const arr of array) {//循环获取子节点
+    const child: RoutesDataItem= {
+      icon: arr.svgIcon,
+      title: `${ arr.title }`,
+      path: `${ arr.path }`,
+      component: ()=> import('@/views/system/menu/list/index.vue'),
+    }
+    menuChildren.push(child);
+  }*/
+
+  const child: RoutesDataItem= {
+    icon: 'permissions',
+    title: 'index-layout.menu.roles',
+    path: 'all',
+    component: ()=> import('@/views/roles/all/index.vue')
+  }
+  menuChildren.push(child);
+  return menuChildren;
+}
+
+// 自定义生成顶部和左边树形菜单结果函数
+export function generateLeftAndTopMenusTree<T>(dataMenus: Array<T>): Array<RoutesDataItem> { //遍历树  获取id数组
+  const menusList: Array<RoutesDataItem> = [];
+  for (let index = 0, len = dataMenus.length; index < len; index += 1) {
+    const menu: any =  dataMenus[index]
+    if(menu.pid===null){ //判断是否为顶层节点
+      const parentMenu: RoutesDataItem= {
+        icon: `${ menu.svgIcon }`,
+        title: menu.title,
+        path: `/${ menu.path }`,
+        component: ()=> import('@/layouts/BlankLayout.vue'),
+        //redirect: '/system/workplace',
+        redirect: '/home/workplace'
+      }
+
+      if(menu.children.length>0){
+        // parentMenu.redirect = "/"+menu.path+"/"+menu.children[0].path
+        parentMenu.children = getLeftMenuChildren(menu.children)
+
+      }
+      menusList.push(parentMenu);
+    }
+
+  }
+  const userMenu =IndexLayoutRoutes.concat(menusList)
+  return userMenu;
 }
 
